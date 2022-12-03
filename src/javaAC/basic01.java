59cac562c25a6925bf2ca51d1400b9200f2ccfce
it-sRohan <rohanchainani15@gmail.com>
2022-12-03 23:47:13 +0530

    sout pattern

--- src/javaAC/basic01.java
+++ src/javaAC/basic01.java
@@ -3,5 +3,10 @@ package javaAC;
 public class basic01 {
     public static void main(String[] args) {
         System.out.println("Hello Rohan!");
+        // basic pattern printing!
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
