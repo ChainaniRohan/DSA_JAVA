8a2c950cc2552697746175cc2364c443c7d05042
it-sRohan <rohanchainani15@gmail.com>
2022-12-04 00:21:38 +0530

    primitive datatype

---
+++ src/javaAC/dataType.java
@@ -0,0 +1,24 @@
+package javaAC;
+
+public class dataType {
+    public static void main(String[] args) {
+        //primitive Datatype!
+        byte b = 3;
+        System.out.println(b+" byte : 1 byte");
+        boolean bool = true;
+        System.out.println(bool+" boolean : 1 byte");
+        long ln =  345678;
+        System.out.println(ln+" long : 8 byte");
+        double dbl = 23456.4567;
+        System.out.println(dbl+" double : 8 byte");
+        short sh = 345;
+        System.out.println(sh+" short : 2 byte");
+        char ch = 'r';
+        System.out.println(ch+" char : 1 byte");
+        float price = 10.5f;
+        System.out.println(price+" price : 4 byte");
+        int i = 34;
+        System.out.println(i+" int : 4 byte");
+
+    }
+}
