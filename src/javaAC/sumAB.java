295616fc146ddcafd4fbc778320426d88396e542
it-sRohan <rohanchainani15@gmail.com>
2022-12-04 12:08:00 +0530

    sum of 2 no.

---
+++ src/javaAC/sumAB.java
@@ -0,0 +1,10 @@
+package javaAC;
+
+public class sumAB {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 8;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
