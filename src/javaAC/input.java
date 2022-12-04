b267cc80b58d891d5237cedd7b3c7f066b6f8a8e
it-sRohan <rohanchainani15@gmail.com>
2022-12-04 12:54:07 +0530

    input types

---
+++ src/javaAC/input.java
@@ -0,0 +1,19 @@
+package javaAC;
+import java.util.*;
+//import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+//        String word = sc.next();
+//        System.out.println(word);
+//        Float price = sc.nextFloat();
+//        System.out.println(price);
+        System.out.println(sc.next());
+        System.out.println(sc.nextShort());
+        System.out.println(sc.nextBoolean());
+        System.out.println(sc.nextDouble());
+    }
+}
